4ff9c72519d098f51d5ba0573e48b9fbc5cdd73e
Kash-ik <Kolesnikovaalina.dom@mail.ru>
2026-05-24 14:57:42 +0400

    Вывод суммы чисел от 1 до N

---
+++ conditional operators/Оператор_Цикла_С_Предусловием.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    int S = 0, N = 10, i = 1;
+    while (i <= N) {
+        S += i;
+        i++;
+    }
+}
